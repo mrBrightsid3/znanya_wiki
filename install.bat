143a2b198b68627b94b67e842d6a39d4eb0fea4c
mrBrightsid3 <andronch2002@gmail.com>
2024-05-07 15:49:34 +0300

    init commit

---
+++ install.bat
@@ -0,0 +1,2 @@
+winget install Microsoft.VisualStudio.2022.BuildTools --force --override "--passive --wait --add Microssft. VisualStudio.Workload. VCTools;includeRecommended"
+rem https://learn.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2022
